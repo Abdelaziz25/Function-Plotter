439d24888ca36d13df5ea3910eea7b9cb4a50d54
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2023-07-18 06:25:40 +0300

    Adding some comments

--- UserInputValidator.py
+++ UserInputValidator.py
@@ -1,10 +1,23 @@
 class InputValidator:
     @staticmethod
     def validate_function(function):
+        """
+        Validates the given function to ensure it contains only allowed characters.
+
+        Parameters:
+            function (str): The function string to validate.
+
+        Returns:
+            bool: True if the function is valid, False otherwise.
+        """
         allowed_chars = set("0123456789x.+-*/^ ")
-        if not function.strip():  # Check if the function is empty or contains only spaces
+
+        # Check if the function is empty or contains only spaces
+        if not function.strip():
             print("Function cannot be empty.")
             return False
+
+        # Check if all characters in the function are allowed
         if set(function) <= allowed_chars:
             return True
         else:
@@ -13,6 +26,16 @@ class InputValidator:
 
     @staticmethod
     def validate_min_max(min_value, max_value):
+        """
+        Validates the minimum and maximum values to ensure they are in the correct order.
+
+        Parameters:
+            min_value (float): The minimum value to validate.
+            max_value (float): The maximum value to validate.
+
+        Returns:
+            bool: True if the values are valid (min_value < max_value), False otherwise.
+        """
         if min_value >= max_value:
             print("Minimum value should be smaller than the maximum value.")
             return False
--- functionplot.py
+++ functionplot.py
@@ -11,11 +11,12 @@ class FunctionPlotterApp(QMainWindow):
         self.setWindowTitle("Function Plotter")
         self.setGeometry(100, 100, 800, 600)
 
+        # Set up the main central widget and layout
         self.central_widget = QWidget(self)
         self.setCentralWidget(self.central_widget)
-
         self.layout = QVBoxLayout(self.central_widget)
 
+        # Create input widgets and plot button
         self.function_label = QLabel("Enter function (e.g., x^2 + 3*x - 5): ")
         self.function_input = QLineEdit()
         self.min_label = QLabel("Enter min value: ")
@@ -24,6 +25,7 @@ class FunctionPlotterApp(QMainWindow):
         self.max_input = QLineEdit()
         self.plot_button = QPushButton("Plot Function")
 
+        # Add input widgets and plot button to the layout
         self.layout.addWidget(self.function_label)
         self.layout.addWidget(self.function_input)
         self.layout.addWidget(self.min_label)
@@ -32,17 +34,20 @@ class FunctionPlotterApp(QMainWindow):
         self.layout.addWidget(self.max_input)
         self.layout.addWidget(self.plot_button)
 
+        # Connect the "Plot Function" button to the plot function
         self.plot_button.clicked.connect(self.on_plot_button_clicked)
 
-        # Initialize the canvas attribute
+        # Initialize the canvas attribute for plotting
         self.canvas = FigureCanvas(Figure())
         self.layout.addWidget(self.canvas)
+
     def on_plot_button_clicked(self):
+        # Get user input from input fields
         function_str = self.function_input.text()
         min_value_str = self.min_input.text()
         max_value_str = self.max_input.text()
 
-        # Validate function and min/max values
+        # Validate function and min/max values using the InputValidator class
         if not InputValidator.validate_function(function_str):
             self.show_error_message("Invalid characters in the function.")
             return
@@ -56,26 +61,30 @@ class FunctionPlotterApp(QMainWindow):
         if not InputValidator.validate_min_max(min_value, max_value):
             self.show_error_message("Invalid min or max value. Minimum value should be smaller than the maximum value.")
             return
+
+        # Replace '^' with '**' for Python evaluation and remove any dots from the function
         function_str = function_str.replace('^', '**')
         function_str = function_str.replace('.', '')
+
+        # Generate x and y values for the function to plot
         x = np.linspace(float(min_value_str), float(max_value_str), 100)
         y = [eval(function_str, {"x": i}) for i in x]
 
-        # Clear any previous plots
+        # Clear any previous plots on the canvas
         self.canvas.figure.clear()
 
-
-        # Create a new plot
+        # Create a new plot on the canvas
         ax = self.canvas.figure.add_subplot(111)
         ax.plot(x, y)
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_title("Function Plot")
 
-        # Draw the new plot
+        # Draw the new plot on the canvas
         self.canvas.draw()
 
     def show_error_message(self, message):
+        # Show an error message in a QMessageBox
         error_box = QMessageBox()
         error_box.setIcon(QMessageBox.Critical)
         error_box.setWindowTitle("Error")
